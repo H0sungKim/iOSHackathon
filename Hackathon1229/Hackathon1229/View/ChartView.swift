63d8c4e071a041043ad17ef3f8d7159ecb83047f
Hosung.Kim <hosungkim5108@gmail.com>
2024-12-29 11:21:51 +0900

    uicolor ext

--- Hackathon1229/Hackathon1229/View/ChartView.swift
+++ Hackathon1229/Hackathon1229/View/ChartView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class ChartView: UIView {
     override init(frame: CGRect) {
@@ -19,6 +20,63 @@ class ChartView: UIView {
     }
     
     private func addComponents() {
+        self.addSubview(navigationBarView)
         
+        navigationBarView.addSubview(btnBack)
+        navigationBarView.addSubview(lbTitle)
+        navigationBarView.addSubview(btnShare)
+        
+        navigationBarView.snp.makeConstraints({ make in
+            make.height.equalTo(64)
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(safeAreaLayoutGuide)
+        })
+        
+        btnBack.snp.makeConstraints({ make in
+            make.leading.equalToSuperview().inset(32)
+            make.centerY.equalToSuperview()
+        })
+        
+        lbTitle.snp.makeConstraints({ make in
+            make.center.equalToSuperview()
+        })
+        
+        btnShare.snp.makeConstraints({ make in
+            make.trailing.equalToSuperview().inset(32)
+            make.centerY.equalToSuperview()
+        })
     }
+    
+    private lazy var navigationBarView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private lazy var btnBack: UIButton = {
+        let button = UIButton()
+        button.setTitle("이전", for: .normal)
+        button.setImage(UIImage(resource: .back), for: .normal)
+        return button
+    }()
+    
+    private lazy var lbTitle: UILabel = {
+        let label = UILabel()
+        label.text = "통계"
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    private lazy var btnShare: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(resource: .share), for: .normal)
+        return button
+    }()
+    
+    private lazy var dayView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 243/255, green: 243/255, blue: 243/255, alpha: 1)
+        return view
+    }()
+    
+    private
 }
