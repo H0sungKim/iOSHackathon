2d560e2e2e19144556e0d331e9cf156339c23ff8
Hosung.Kim <hosungkim5108@gmail.com>
2024-12-29 10:58:55 +0900

    add packages

--- Hackathon1229/Hackathon1229/ViewController/ChartViewController.swift
+++ Hackathon1229/Hackathon1229/ViewController/ChartViewController.swift
@@ -12,18 +12,12 @@ class ChartViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view = chartView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    private lazy var chartView: ChartView = {
+        let chartView = ChartView()
+        return chartView
+    }()
 
 }
